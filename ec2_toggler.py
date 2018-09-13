1e55c30debb2b9ead9bbcc254a235e77984f5840
ArtemF <zakatsv@gmail.com>
2018-09-13 21:46:42 +0300

    added a func to check time

--- ec2_toggler.py
+++ ec2_toggler.py
@@ -11,32 +11,47 @@ import json
 import datetime
 import re
 
-code = ''
-tag = ''
+status_code = ''
+work_hours = ''
+now = ''
+start_time = ''
+stop_time = ''
+
 
 ins_id = 'i-0ea7e0158c98e3765'
 cli_get_status = "aws ec2 describe-instance-status --include-all-instances --instance-id " + ins_id
 cli_get_tag = "aws ec2 describe-tags --filters \"Name=resource-id,Values=" + ins_id + "\""
-#print cli_get_tag
+cli_start_ins = "aws ec2 start-instances --instance-ids " + ins_id
+cli_stop_ins = "aws ec2 stop-instances --instance-ids " + ins_id
 
 def get_status_code():
-  global code
+  global status_code
   cli_output = subprocess.check_output(['bash','-c', cli_get_status])
   data = json.loads(cli_output)
-  code = data['InstanceStatuses'][0]['InstanceState']['Code']
+  status_code = data['InstanceStatuses'][0]['InstanceState']['Code']
 
 def get_work_hours():
-  global tag
+  global work_hours
   cli_output = subprocess.check_output(['bash','-c', cli_get_tag])
   data = json.loads(cli_output)
-  tag = data['Tags'][1]['Value']
+  work_hours = data['Tags'][1]['Value']
+
+def analyze_work_hours():
+  global work_hours
+  global now
+  global start_time
+  global stop_time
+  now = datetime.datetime.now()
+  stamps = re.split(r'[-:]', work_hours)
+  start_time = now.replace(hour=int(stamps[0]), minute=int(stamps[1]))
+  stop_time = now.replace(hour=int(stamps[2]), minute=int(stamps[3]))
 
 if __name__ == "__main__":
   get_status_code()
   get_work_hours()
-  print code
-  print tag
-
-
-
-#if __name__ == "__main__":
+  analyze_work_hours()
+  print status_code
+  print work_hours
+  print now
+  print start_time
+  print stop_time
